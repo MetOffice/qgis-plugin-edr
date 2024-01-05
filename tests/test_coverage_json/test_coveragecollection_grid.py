bde064b20804a0cf51b0c209e63f2ddeede3233a
Jan Caha <jan.caha@lutraconsulting.co.uk>
2024-01-05 10:08:59 +0100

    coverage collection grids tests

---
+++ tests/test_coverage_json/test_coveragecollection_grid.py
@@ -0,0 +1,41 @@
+import typing
+
+import numpy as np
+from qgis.core import QgsCoordinateReferenceSystem, QgsMapLayer, QgsRasterLayer
+from qgis.PyQt.QtCore import QDateTime
+
+from edr_plugin.coveragejson.coverage import Coverage
+from edr_plugin.coveragejson.coverage_json_reader import CoverageJSONReader
+from edr_plugin.coveragejson.utils import ArrayWithTZ
+
+
+def test_grid(data_dir):
+    filename = data_dir / "coveragecollection_grids.covjson"
+
+    assert filename.exists()
+
+    coverage_json = CoverageJSONReader(filename)
+
+    assert coverage_json.referencing
+
+    assert (
+        coverage_json.crs().toWkt()
+        == 'GEOGCS["Unknown",DATUM["Unknown",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.017453]]'
+    )
+
+    assert coverage_json.domain_type == "Grid"
+    assert coverage_json.coverages_count == 4
+
+    coverages = coverage_json.coverages
+
+    assert all(isinstance(c, Coverage) for c in coverages)
+
+    for i in range(4):
+        coverage = coverages[i]
+        assert isinstance(coverage, Coverage)
+        assert len(coverage.parameter_names) == 1
+
+        param_name = coverage.parameter_names[0]
+
+        assert len(coverage.raster_layers(param_name)) == 1
+        assert all(isinstance(layer, QgsRasterLayer) for layer in coverage.raster_layers(param_name))
